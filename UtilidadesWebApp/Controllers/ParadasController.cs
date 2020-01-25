7208481a5e7d011e53a194dff9a15c7a1ddec9e5
David Rubio Lastanao <drlastanao@hotmail.com>
2020-01-25 10:00:55 +0100

    Primera versión

---
+++ UtilidadesWebApp/Controllers/ParadasController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UtilidadesWebApp.Controllers
+{
+    public class ParadasController : Controller
+    {
+        // GET: Paradas
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Paradas/Details/5
+        public ActionResult Cercanas(string posicion)
+        {
+            return View();
+        }
+    }
+}
