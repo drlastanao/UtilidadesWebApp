d9f24c683c8a77ff4e369d49519f95ce831dfd44
David Rubio Lastanao <drlastanao@hotmail.com>
2020-01-25 15:56:36 +0100

    Paradas hechas

--- UtilidadesWebApp/Controllers/ParadasController.cs
+++ UtilidadesWebApp/Controllers/ParadasController.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using UtilidadesWebApp.Models;
 
 namespace UtilidadesWebApp.Controllers
 {
@@ -16,9 +18,107 @@ namespace UtilidadesWebApp.Controllers
         }
 
         // GET: Paradas/Details/5
-        public ActionResult Cercanas(string posicion)
+        public ActionResult Cercanas(string posicion, string parada, string guardar)
         {
-            return View();
+            var cookie = Request.Cookies["Paradas"] != null ? JsonConvert.DeserializeObject<List<Parada>>(Request.Cookies["Paradas"]) : null;
+
+            if (!string.IsNullOrEmpty(parada))
+            {
+                TratarCookies(posicion, parada, guardar, cookie);
+                return new RedirectResult("http://zaragoza.avanzagrupo.com/frm_esquemaparadatime.php?poste=" + parada);
+            }
+
+            if (cookie != null)
+            {
+                var cookies = OrdenarCookies(cookie, posicion);
+
+                return View("Cercanas", cookies);
+            }
+            else
+                return NotFound();
+        }
+
+        private IOrderedEnumerable<Parada> OrdenarCookies(List<Parada> cookie, string posicion)
+        {
+            float latitud = (posicion != null) ? float.Parse(posicion.Split('|')[0]) : 0;
+            float longitud = (posicion != null) ? float.Parse(posicion.Split('|')[1]) : 0;
+
+
+            foreach (var parada in cookie)
+            {
+
+                float difLatitud = parada.latitud - latitud;
+                float difLongitud = parada.longitud - longitud;
+
+
+                double a = Math.Sin(difLatitud / 2) * Math.Sin(difLatitud / 2) +
+                          Math.Cos(EnRadianes(latitud)) *
+                          Math.Cos(EnRadianes(parada.latitud)) *
+                          Math.Sin(difLongitud / 2) * Math.Sin(difLongitud / 2);
+                double diferencia = 1 - a;
+
+                double c = 2 * Math.Atan2(Math.Sqrt(a), Math.Sqrt(diferencia));
+
+
+                parada.distancia = (float)c;
+
+            }
+
+
+            var aux = cookie.OrderBy(x => x.distancia);
+            return aux;
+        }
+
+        static float EnRadianes(float valor)
+        {
+            return Convert.ToSingle(Math.PI / 180) * valor;
+        }
+        private void TratarCookies(string posicion, string parada, string guardar, List<Parada> cookie)
+        {
+            if (!string.IsNullOrEmpty(guardar))
+            {
+
+                var aux2 = new Parada()
+                {
+                    numero = parada,
+                    latitud = (posicion != null) ? float.Parse(posicion.Split('|')[0]) : 0,
+                    longitud = (posicion != null) ? float.Parse(posicion.Split('|')[1]) : 0,
+                    distancia = 9999999
+
+                };
+
+                if (cookie == null)
+                {
+                    var aux = new List<Parada>();
+                    aux.Add(aux2);
+                    Set("Paradas", JsonConvert.SerializeObject(aux));
+
+                }
+                else
+                {
+                    if (cookie.Where(x => x.numero == parada).FirstOrDefault() == null)
+                    {
+                        cookie.Add(aux2);
+                        Set("Paradas", JsonConvert.SerializeObject(cookie));
+                    }
+                }
+
+            }
+        }
+
+
+        public void Set(string key, string value, int? expireTime = null)
+        {
+            CookieOptions option = new CookieOptions();
+
+            if (expireTime.HasValue)
+                option.Expires = DateTime.Now.AddMinutes(expireTime.Value);
+            else
+                option.Expires = DateTime.Now.AddMilliseconds(1000 * 60 * 60 * 24 * 10);
+
+            Response.Cookies.Append(key, value, option);
         }
     }
+
+
 }
---
+++ UtilidadesWebApp/Models/Parada.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UtilidadesWebApp.Models
+{
+    public class Parada
+    {
+        public string numero { get; set; }
+        public float latitud { get; set; }
+        public float longitud { get; set; }
+        public float distancia  { get; set; }
+
+    }
+}
